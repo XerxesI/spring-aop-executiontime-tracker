b311ee2c750deee4777b901646d11763cf518d1b
Meelis Kivimäe <meelis.kivimae@gmail.com>
2022-08-19 08:17:29 +0300

    The very first commit

--- src/main/java/com/javatechie/spring/aop/api/advice/ExecutionTimeTrackerAdvice.java
+++ src/main/java/com/javatechie/spring/aop/api/advice/ExecutionTimeTrackerAdvice.java
@@ -3,6 +3,7 @@ package com.javatechie.spring.aop.api.advice;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -12,8 +13,12 @@ import org.springframework.stereotype.Component;
 public class ExecutionTimeTrackerAdvice {
 	
 	Logger logger=LoggerFactory.getLogger(ExecutionTimeTrackerAdvice.class);
-	
-	@Around("@annotation(com.javatechie.spring.aop.api.advice.TrackExecutionTime)")
+
+	@Pointcut("execution(* *(..))")
+	public void methodsToBeProfiled(){
+	}
+
+	@Around("methodsToBeProfiled() && @annotation(com.javatechie.spring.aop.api.advice.TrackExecutionTime)")
 	public Object trackTime(ProceedingJoinPoint pjp) throws Throwable {
 		long stratTime=System.currentTimeMillis();
 		Object obj=pjp.proceed();
--- src/main/java/com/javatechie/spring/aop/api/service/ProductService.java
+++ src/main/java/com/javatechie/spring/aop/api/service/ProductService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Random;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,26 +17,32 @@ import com.javatechie.spring.aop.api.repository.ProductRepository;
 @Service
 public class ProductService {
 
-	@Autowired
-	private ProductRepository repository;
+    @Autowired
+    private ProductRepository repository;
 
-	@PostConstruct
-	public void initDB() {
-		List<Product> list=new ArrayList<>();
-		for(int i=1;i<=10000;i++) {
-			list.add(new Product(i, "product"+i, new Random().nextInt(2000)));
-		}
-		repository.saveAll(list);
-	}
-	
-	@TrackExecutionTime
-	public List<Product> addProduct(List<Product> products) {
-		return repository.saveAll(products);
-	}
+    @PostConstruct
+    public void initDB() {
+        List<Product> list = new ArrayList<>();
+        for (int i = 1; i <= 10000; i++) {
+            list.add(new Product(i, "product" + i, new Random().nextInt(2000)));
+        }
+        repository.saveAll(list);
+    }
 
-	@TrackExecutionTime
-	public List<Product> findAllProducts() {
-		return repository.findAll();
-	}
+    @TrackExecutionTime
+    public List<Product> addProduct(List<Product> products) {
+        return repository.saveAll(products);
+    }
+
+    @TrackExecutionTime
+    public List<Product> findAllProducts() {
+        nestedMethod();
+        return repository.findAll();
+    }
+
+    @TrackExecutionTime
+    void nestedMethod() {
+        System.out.println("Nested method call");
+    }
 
 }
